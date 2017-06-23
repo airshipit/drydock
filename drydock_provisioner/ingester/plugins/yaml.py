5d1f8575245f2fe79000ef106beec55a7f19bddc
Scott Hussey <sh8121@att.com>
2017-06-23 09:06:50 -0500

    Revert "Fix typo"
    This reverts commit c3ed51dfd37831d1ce33c1af645ec89e48a206b2.

--- drydock_provisioner/ingester/plugins/yaml.py
+++ drydock_provisioner/ingester/plugins/yaml.py
@@ -371,6 +371,6 @@ class YamlIngester(IngesterPlugin):
                     target = metadata.get('target', 'all')
                     name = metadata.get('name', None)
 
-                    model = objects.PromenadeConfig(target=target, name=name, kind=kind, document=yaml.dump(d, default_flow_style=False))
+                    model = objects.PromenadeConfig(target=target, name=name, kind=kind, document=yaml.dump(d))
                     models.append(model)
         return models
