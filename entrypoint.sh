24e8bd1a3a41ce1ed8eb47afd44115f9d7919aa8
Bryan Strassner <bryan.strassner@gmail.com>
2018-02-07 13:38:13 -0600

    uWSGI http-timeout option
    Adds a value for http-timeout for drydock, in support
    of the long sync /validatedesign invocation without
    disconnecting the client.
    
    Change-Id: I482dec7092ca4204accf13913621c7b2e16eeaf9

--- entrypoint.sh
+++ entrypoint.sh
@@ -7,6 +7,8 @@ PORT=${PORT:-9000}
 DRYDOCK_API_WORKERS=${DRYDOCK_API_WORKERS:-"1"}
 # Threads per worker
 DRYDOCK_API_THREADS=${DRYDOCK_API_THREADS:-"4"}
+# HTTP timeout
+HTTP_TIMEOUT=${HTTP_TIMEOUT:-"600"}
 
 if [ "$1" = 'server' ]; then
 # Run Drydock under uWSGI
@@ -17,7 +19,8 @@ if [ "$1" = 'server' ]; then
 #   --pyargs - Provide some command line arguments to the Python executable
 #   --threads - Number of threads each uWSGI worker should use for handling requests
 #   --workers - Number of uWSGI workers/processes for handling requests
-    exec uwsgi --http :${PORT} --paste config:/etc/drydock/api-paste.ini --enable-threads -L --pyargv "--config-file /etc/drydock/drydock.conf" --threads $DRYDOCK_API_THREADS --workers $DRYDOCK_API_WORKERS
+#   --http-timeout - How long uWSGI will proxy the python code processing before disconnecting a client
+    exec uwsgi --http :${PORT} --paste config:/etc/drydock/api-paste.ini --enable-threads -L --pyargv "--config-file /etc/drydock/drydock.conf" --threads $DRYDOCK_API_THREADS --workers $DRYDOCK_API_WORKERS --http-timeout $HTTP_TIMEOUT
 fi
 
 exec ${CMD} $@
