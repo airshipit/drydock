812cef9335c1cf0f06336d2554cb48db3f69ff06
Scott Hussey <sh8121@att.com>
2018-07-31 13:23:51 -0500

    (zuul) Improve image building
    Improve image building to get closer to
    support zuul package mirrors
    
    Change-Id: I57285b242ddc50e8e902ad2820982a2d4b351bc3

---
+++ tools/drydock_image_build.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+set -x
+
+UBUNTU_REPO=${UBUNTU_REPO:-""}
+TRUSTED_UBUNTU_REPO=${TRUSTED_UBUNTU_REPO:-"no"}
+ALLOW_UNATHENTICATED=${ALLOW_UNAUTHENTICATED:-"false"}
+PIP_INDEX_URL=${PIP_INDEX_URL:-""}
+PIP_TRUSTED_HOST=${PIP_TRUSTED_HOST:-""}
+
+ADDL_BUILD_ARGS=""
+
+if [[ ! -z "${UBUNTU_REPO}" ]]
+then
+  ADDL_BUILD_ARGS="${ADDL_BUILD_ARGS} --build-arg UBUNTU_REPO=${UBUNTU_REPO}"
+  ADDL_BUILD_ARGS="${ADDL_BUILD_ARGS} --build-arg TRUSTED_UBUNTU_REPO=${TRUSTED_UBUNTU_REPO}"
+  ADDL_BUILD_ARGS="${ADDL_BUILD_ARGS} --build-arg ALLOW_UNAUTHENTICATED=${ALLOW_UNAUTHENTICATED}"
+fi
+
+if [[ ! -z "${PIP_INDEX_URL}" ]]
+then
+  ADDL_BUILD_ARGS="${ADDL_BUILD_ARGS}| --build-arg PIP_INDEX_URL=${PIP_INDEX_URL}"
+  ADDL_BUILD_ARGS="${ADDL_BUILD_ARGS}| --build-arg PIP_TRUSTED_HOST=${PIP_TRUSTED_HOST}"
+fi
+
+docker build --network host -t ${IMAGE} --label ${LABEL} -f images/drydock/Dockerfile \
+  ${ADDL_BUILD_ARGS} \
+  --build-arg http_proxy=${http_proxy} \
+  --build-arg https_proxy=${https_proxy} \
+  --build-arg HTTP_PROXY=${HTTP_PROXY} \
+  --build-arg HTTPS_PROXY=${HTTPS_PROXY} \
+  --build-arg no_proxy=${no_proxy} \
+  --build-arg NO_PROXY=${NO_PROXY} .
+
