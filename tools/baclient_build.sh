6609d561e1b75afeca04273c44fc2750731e1e1e
Sergiy Markin <smarkin@mirantis.com>
2023-05-31 14:33:23 +0000

    Fix go binary path
    This PS fixed Go binary path in some bionic image docker build process.
    
    Change-Id: I9975b4898b7cd312672bbad92765bc2ab5ed3089

--- tools/baclient_build.sh
+++ tools/baclient_build.sh
@@ -7,7 +7,9 @@ if $(uname -a | grep -q Ubuntu); then
   GOPATH=$1
   BUILD_DIR=$2
   if [[ ! -f ./baclient_built ]]; then
-    GO111MODULE=off GOPATH=${GOPATH} go build -v -o ${BUILD_DIR}/baclient baclient
+    whereis go
+    go env
+    GO111MODULE=off GOPATH=${GOPATH} /usr/bin/go build -v -o ${BUILD_DIR}/baclient baclient
   else
     echo "Baclient library is already built. No action."
   fi
